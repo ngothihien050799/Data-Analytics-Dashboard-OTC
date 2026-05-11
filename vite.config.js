7655c2a498ff40016a81d2e34138897ea9320f6f
NgoHien <ngohien311299@gmail.com>
2026-05-12 00:11:32 +0700

    Add Vite proxy for local dev

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/process': 'http://127.0.0.1:5000',
+      '/config': 'http://127.0.0.1:5000'
+    }
+  }
 })
